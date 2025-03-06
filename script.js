89b132a7e0d965a8d147d56c6524052b70711658
Nadrojria <loicdesbordes@yahoo.fr>
2025-03-06 12:12:05 +0100

    step 5 : 4 joueurs OK // CSS // manque selection du nb de joueurs

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-/******RECUP**** */
+/******RECUP HTML**** */
+const displayGame = document.getElementById('playerAndMatches'); // englobe nb allumettes et joueurs
 const displayMatches = document.getElementById('numOfMatches');
 const playerOne = document.getElementById('playerOne');
 const playerTwo = document.getElementById('playerTwo');
+const playerThree = document.getElementById('playerThree');
+const playerFour = document.getElementById('playerFour');
 const namePlayerOne = document.getElementById('namePlayerOne');
 const namePlayerTwo = document.getElementById('namePlayerTwo');
+const namePlayerThree = document.getElementById('namePlayerThree');
+const namePlayerFour = document.getElementById('namePlayerFour');
 
 let totalMatches = 20;
 
 /******FONCTION SECONDAIRES*****
 *****************************/
+function displayPlayer () {
+    displayGame.style.display = 'none';
+//     displayMatches.style.display = 'none';
+//     playerOne.style.display = 'none';
+//     namePlayerOne.style.display = 'none';
+//     playerTwo.style.display = 'none';
+//     namePlayerTwo.style.display = 'none';
+//     playerThree.style.display = 'none';
+//     namePlayerThree.style.display = 'none';
+//     playerFour.style.display = 'none';
+//     namePlayerFour.style.display = 'none';
+}
 
 function removeMatches (matchesTotal, matchesPlayer) {
     let result = matchesTotal - matchesPlayer;
@@ -20,24 +37,28 @@ function winning (player) {
         displayMatches.innerText = `${player} remporte la partie !!!`;
         playerOne.disabled = true;
         playerTwo.disabled = true;
+        playerThree.disabled = true;
+        playerFour.disabled = true;
     }
 }
 
-function disabledPlayer (player1, player2) {
-    player1.disabled = true;
-    player2.disabled = false;
+function disabledPlayer (player, nextPlayer, nonePlayerB, nonePlayerC) {
+    player.disabled = true;
+    nextPlayer.disabled = false;
+    nonePlayerB.disabled = true;
+    nonePlayerC.disabled = true;
 }
 
 /******FONCTION PRINCIPALE*****
  *****************************/
-function playerChoice (player, nonePlayer) {
+function playerChoice (player, nextPlayer, nonePlayerB, nonePlayerC) {
     const choice = player.value; // stock le choix du select dans choice
     if (player.value > totalMatches) {
         displayMatches.innerText = `Il reste ${totalMatches} allumettes, tu dois donc prendre un nombre d'allumettes inférieur ou égal à ${totalMatches}`;
     } else {
         totalMatches = removeMatches(totalMatches, parseInt(choice)); //parseInt change le "1" en 1.
         displayMatches.innerText = `Il reste ${totalMatches} allumettes`;
-        disabledPlayer(player, nonePlayer);
+        disabledPlayer(player, nextPlayer, nonePlayerB, nonePlayerC);
     }
     player.value = ""; // remet le choix défault du select après avoir fait son choix
 }
@@ -45,13 +66,25 @@ function playerChoice (player, nonePlayer) {
 /***********LANCEMENT********
 ******************************/
 playerOne.addEventListener("change", () => {
-    playerChoice(playerOne, playerTwo);
+    playerChoice(playerOne, playerTwo, playerThree, playerFour);
     winning(namePlayerOne.innerText);
 });
 playerTwo.addEventListener("change", () => {
-    playerChoice(playerTwo, playerOne);
+    playerChoice(playerTwo, playerThree, playerFour, playerOne);
     winning(namePlayerTwo.innerText);
 });
+playerThree.addEventListener("change", () => {
+    playerChoice(playerThree, playerFour, playerOne, playerTwo);
+    winning(namePlayerThree.innerText);
+});
+playerFour.addEventListener("change", () => {
+    playerChoice(playerFour, playerOne, playerTwo, playerThree);
+    winning(namePlayerFour.innerText);
+});
+
+
+
+
 
 
 
