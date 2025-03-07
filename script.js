fef74ccd77edbcce783587ee06f800d9775c9fde
Nadrojria <loicdesbordes@yahoo.fr>
2025-03-07 13:33:25 +0100

    4 joueurs dynamiques / manque notion de tour qui permettrait d'activer ou désactiver le select des joueurs

--- script.js
+++ script.js
@@ -1,86 +1,105 @@
 /******RECUP HTML**** */
-const displayGame = document.getElementById('playerAndMatches'); // englobe nb allumettes et joueurs
+const containerPlayer = document.getElementById('displayContainerPlayer');
+const howMuchPlayer = document.getElementById('HowMuchPlayers');
+const numOfPlayers = document.getElementById('numOfPlayers');
 const displayMatches = document.getElementById('numOfMatches');
-const playerOne = document.getElementById('playerOne');
-const playerTwo = document.getElementById('playerTwo');
-const playerThree = document.getElementById('playerThree');
-const playerFour = document.getElementById('playerFour');
-const namePlayerOne = document.getElementById('namePlayerOne');
-const namePlayerTwo = document.getElementById('namePlayerTwo');
-const namePlayerThree = document.getElementById('namePlayerThree');
-const namePlayerFour = document.getElementById('namePlayerFour');
 
 let totalMatches = 20;
 
 /******FONCTION SECONDAIRES*****
 *****************************/
-function displayPlayer () {
-    displayGame.style.display = 'none';
-//     displayMatches.style.display = 'none';
-//     playerOne.style.display = 'none';
-//     namePlayerOne.style.display = 'none';
-//     playerTwo.style.display = 'none';
-//     namePlayerTwo.style.display = 'none';
-//     playerThree.style.display = 'none';
-//     namePlayerThree.style.display = 'none';
-//     playerFour.style.display = 'none';
-//     namePlayerFour.style.display = 'none';
-}
-
 function removeMatches (matchesTotal, matchesPlayer) {
     let result = matchesTotal - matchesPlayer;
     return result;
 }
 
-function winning (player) {
+function winning (labelPlayer) {
     if (totalMatches == 0) {
-        displayMatches.innerText = `${player} remporte la partie !!!`;
-        playerOne.disabled = true;
-        playerTwo.disabled = true;
-        playerThree.disabled = true;
-        playerFour.disabled = true;
+        displayMatches.innerText = `${labelPlayer} remporte la partie !!!`;
+        containerPlayer.style.display = 'none'; // retire l'interface des joueurs
     }
 }
 
-function disabledPlayer (player, nextPlayer, nonePlayerB, nonePlayerC) {
+function disabledPlayer (player) {
     player.disabled = true;
-    nextPlayer.disabled = false;
-    nonePlayerB.disabled = true;
-    nonePlayerC.disabled = true;
+}
+
+function createPlayers () {
+    array.forEach(elem => {
+        let divPlayer = document.createElement('div');
+        containerPlayer.appendChild(divPlayer);
+        divPlayer.classList.add("allSelect");
+
+        let labelPlayer = document.createElement('label');
+        divPlayer.appendChild(labelPlayer);
+        labelPlayer.classList.add("labelPlayer");
+        labelPlayer.setAttribute("id", `label${elem}`)
+        labelPlayer.innerText = `Joueur ${elem}`;
+        
+        let selectPlayer = document.createElement('select');
+        divPlayer.appendChild(selectPlayer);
+        selectPlayer.setAttribute("id", `select${elem}`);
+        
+
+        for (let j = 0; j <= 6; j++) {
+            let optionPlayer = document.createElement('option');
+            if (j == 0) {
+                optionPlayer.setAttribute("value", "");
+                optionPlayer.innerText = '--Make a choice--';
+            } else {
+            optionPlayer.setAttribute("value", j);
+            optionPlayer.innerText = j;
+            }
+            selectPlayer.appendChild(optionPlayer);
+        }
+
+        selectPlayer.addEventListener("change", () => {
+            playerChoice(selectPlayer);
+            winning(labelPlayer.innerText);
+        })
+    })
 }
 
 /******FONCTION PRINCIPALE*****
  *****************************/
-function playerChoice (player, nextPlayer, nonePlayerB, nonePlayerC) {
+function playerChoice (player) {
     const choice = player.value; // stock le choix du select dans choice
-    if (player.value > totalMatches) {
+    if (parseInt(choice) > totalMatches) { // parseInt change le "1" de la value html en 1.
         displayMatches.innerText = `Il reste ${totalMatches} allumettes, tu dois donc prendre un nombre d'allumettes inférieur ou égal à ${totalMatches}`;
     } else {
-        totalMatches = removeMatches(totalMatches, parseInt(choice)); //parseInt change le "1" en 1.
+        totalMatches = removeMatches(totalMatches, parseInt(choice)); //
         displayMatches.innerText = `Il reste ${totalMatches} allumettes`;
-        disabledPlayer(player, nextPlayer, nonePlayerB, nonePlayerC);
     }
     player.value = ""; // remet le choix défault du select après avoir fait son choix
 }
  
 /***********LANCEMENT********
 ******************************/
-playerOne.addEventListener("change", () => {
-    playerChoice(playerOne, playerTwo, playerThree, playerFour);
-    winning(namePlayerOne.innerText);
+let array = [];
+
+numOfPlayers.addEventListener("change", () => {
+    const players = numOfPlayers.value;
+    for (let i = 1; i <= parseInt(players); i++) {
+        array.push(i);
+    }
+    createPlayers();
+    howMuchPlayer.style.display = 'none';
+    numOfPlayers.style.display = 'none';
 });
-playerTwo.addEventListener("change", () => {
-    playerChoice(playerTwo, playerThree, playerFour, playerOne);
-    winning(namePlayerTwo.innerText);
-});
-playerThree.addEventListener("change", () => {
-    playerChoice(playerThree, playerFour, playerOne, playerTwo);
-    winning(namePlayerThree.innerText);
-});
-playerFour.addEventListener("change", () => {
-    playerChoice(playerFour, playerOne, playerTwo, playerThree);
-    winning(namePlayerFour.innerText);
-});
+
+
+/********REJOUER ********
+*************************/
+
+/******Notion de tour****
+
+Cela permettrait d'activer un joueur et désactiver les autres.
+On pourrait initiliaser une variable turn à 1, et la comparer à l'index du tableau du nombre de joueurs. Si c'est égale on active le joueur de l'index et desactive les autres.
+A chaque AEL on fait un turn++. Si turn++ est supérieur ou égale à nombre de joueurs total, on remet turn = 1;
+ 
+**************************/
+
+
 
 
 
